9e63c70328b95929984a218e33c8b252207b4846
zhm426s <zhildenminton1@student.gsu.edu>
2026-05-04 15:50:47 -0400

    Add tile images for bloom theme

--- index.php
+++ index.php
@@ -43,21 +43,21 @@ if (isset($_POST['name']) && $_POST['name'] !== ''){
             <button type="button">Start Game</button> <!--attach this to a script for beginning the game-->
             <div id="board-center">
                 <div id="game-board"> <!-- pieces may have to be loaded dynamically, pos-# classes are for where in the board they sit-->
-                    <div class="board-piece pos-1 bloom" id="1"><p>1</p></div>
-                    <div class="board-piece pos-2 bloom" id="2"><p>2</p></div>
-                    <div class="board-piece pos-3 bloom" id="3"><p>3</p></div>
-                    <div class="board-piece pos-4 bloom" id="4"><p>4</p></div>
-                    <div class="board-piece pos-5 bloom" id="5"><p>5</p></div>
-                    <div class="board-piece pos-6 bloom" id="6"><p>6</p></div>
-                    <div class="board-piece pos-7 bloom" id="7"><p>7</p></div>
-                    <div class="board-piece pos-8 bloom" id="8"><p>8</p></div>
-                    <div class="board-piece pos-9 bloom" id="9"><p>9</p></div>
-                    <div class="board-piece pos-10 bloom" id="10"><p>10</p></div>
-                    <div class="board-piece pos-11 bloom" id="11"><p>11</p></div>
-                    <div class="board-piece pos-12 bloom" id="12"><p>12</p></div>
-                    <div class="board-piece pos-13 bloom" id="13"><p>13</p></div>
-                    <div class="board-piece pos-14 bloom" id="14"><p>14</p></div>
-                    <div class="board-piece pos-15 bloom" id="15"><p>15</p></div>
+                    <div class="board-piece pos-1 bloom" id="p1"><p>1</p></div>
+                    <div class="board-piece pos-2 bloom" id="p2"><p>2</p></div>
+                    <div class="board-piece pos-3 bloom" id="p3"><p>3</p></div>
+                    <div class="board-piece pos-4 bloom" id="p4"><p>4</p></div>
+                    <div class="board-piece pos-5 bloom" id="p5"><p>5</p></div>
+                    <div class="board-piece pos-6 bloom" id="p6"><p>6</p></div>
+                    <div class="board-piece pos-7 bloom" id="p7"><p>7</p></div>
+                    <div class="board-piece pos-8 bloom" id="p8"><p>8</p></div>
+                    <div class="board-piece pos-9 bloom" id="p9"><p>9</p></div>
+                    <div class="board-piece pos-10 bloom" id="p10"><p>10</p></div>
+                    <div class="board-piece pos-11 bloom" id="p11"><p>11</p></div>
+                    <div class="board-piece pos-12 bloom" id="p12"><p>12</p></div>
+                    <div class="board-piece pos-13 bloom" id="p13"><p>13</p></div>
+                    <div class="board-piece pos-14 bloom" id="p14"><p>14</p></div>
+                    <div class="board-piece pos-15 bloom" id="p15"><p>15</p></div>
                 </div>
             </div>
             <div id="stats">
